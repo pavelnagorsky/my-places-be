a26be72d547d5e482959b60d5792aacc5217be0d
Pavel Nagorsky <pavelnagorsky@mail.ru>
2024-04-20 19:56:18 +0300

    added drop cache api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, Post } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Auth } from './modules/auth/decorators/auth.decorator';
+import { RoleNamesEnum } from './modules/roles/enums/role-names.enum';
 
 @ApiTags('System')
 @Controller()
 export class AppController {
+  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+
   @ApiOperation({ summary: 'Check server status' })
   @ApiOkResponse({
     description: 'Server is working',
@@ -11,5 +16,16 @@ export class AppController {
   @Get('status')
   getStatus(): string {
     return 'OK';
+  }
+
+  @ApiOperation({ summary: 'Drop all cache' })
+  @ApiOkResponse({
+    description: 'Cache dropped',
+  })
+  @Auth(RoleNamesEnum.ADMIN)
+  @Post('drop-cache')
+  async dropAllCache() {
+    await this.cacheManager.reset();
+    return;
   }
 }
