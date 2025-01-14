8cdafd1cbbbe897b59672ffa593fa2218c9840cc
Pavel Nagorsky <pavelnagorsky@asist-lab.com>
2025-01-14 18:01:45 +0300

    work on routes module

--- src/modules/routes/routes.service.ts
+++ src/modules/routes/routes.service.ts
@@ -2,9 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import {
+  Between,
+  Equal,
+  ILike,
+  LessThanOrEqual,
+  MoreThanOrEqual,
+  Repository,
+} from 'typeorm';
 import { Route } from './entities/route.entity';
 import { User } from '../users/entities/user.entity';
+import { AccessTokenPayloadDto } from '../auth/dto/access-token-payload.dto';
+import { RoutesListRequestDto } from './dto/routes-list-request.dto';
+import { RoutesListOrderByEnum } from './enums/routes-list-order-by.enum';
 
 @Injectable()
 export class RoutesService {
@@ -13,23 +23,87 @@ export class RoutesService {
     private routesRepository: Repository<Route>,
   ) {}
 
-  async create(createRouteDto: CreateRouteDto, user: User) {
-    return 'This action adds a new route';
+  async create(dto: CreateRouteDto, user: User) {
+    const route = this.routesRepository.create({
+      ...dto,
+      places: dto.placeIds.map((id) => ({ id })),
+      author: user,
+    });
+
+    const { id } = await this.routesRepository.save(route);
+    return id;
   }
 
-  findAll() {
-    return `This action returns all routes`;
+  async findMyRoutes(
+    langId: number,
+    dto: RoutesListRequestDto,
+    tokenPayload: AccessTokenPayloadDto,
+  ) {
+    const orderDirection = dto.orderAsc ? 'ASC' : 'DESC';
+
+    const getDateWhereOption = () => {
+      if (!!dto.dateFrom && !!dto.dateTo)
+        return Between(new Date(dto.dateFrom), new Date(dto.dateTo));
+      if (!!dto.dateFrom) return MoreThanOrEqual(new Date(dto.dateFrom));
+      if (!!dto.dateTo) return LessThanOrEqual(new Date(dto.dateTo));
+      return undefined;
+    };
+
+    const res = await this.routesRepository.findAndCount({
+      skip: dto.page * dto.pageSize,
+      take: dto.pageSize,
+      order: {
+        createdAt:
+          dto.orderBy === RoutesListOrderByEnum.CREATED_AT || !dto.orderBy
+            ? orderDirection
+            : undefined,
+      },
+      relations: { places: true },
+      where: {
+        author: {
+          id: tokenPayload.id,
+        },
+        createdAt: getDateWhereOption(),
+        title:
+          !!dto.search && dto.search.length > 0
+            ? ILike(`%${dto.search}%`)
+            : undefined,
+      },
+    });
+
+    return res;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} route`;
   }
 
-  async update(id: number, updateRouteDto: UpdateRouteDto, user: User) {
-    return `This action updates a #${id} route`;
+  async update(id: number, dto: UpdateRouteDto) {
+    const route = this.routesRepository.create({
+      ...dto,
+      places: dto.placeIds.map((id) => ({ id })),
+      id,
+    });
+
+    await this.routesRepository.save(route);
+    return id;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} route`;
+  async remove(id: number) {
+    const deleted = await this.routesRepository.remove(
+      this.routesRepository.create({ id }),
+    );
+    return { id };
+  }
+
+  async checkUserRelation(userId: number, routeId: number) {
+    return await this.routesRepository.exists({
+      where: {
+        author: {
+          id: Equal(userId),
+        },
+        id: Equal(routeId),
+      },
+    });
   }
 }
